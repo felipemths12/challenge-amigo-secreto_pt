087529036f737e0966b171e73972ef1cb5ecbdab
Felipe <felipe.matheuss@gmail.com>
2025-03-11 19:23:17 -0300

    lógica aprimorada

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ function nomesAmigos(){
     // Limpa a lista existente
     lista.innerHTML = "";
 
-    // Percorre o array de amigos e cria um <li> para cada nome
+    // Percorre o array de amigos e cria um li para cada nome
     for (let i = 0; i < amigos.length; i++) {
         const li = document.createElement("li");
         li.textContent = amigos[i]; // Define o nome do amigo no item da lista
-        lista.appendChild(li); // Adiciona o <li> à lista HTML
+        lista.appendChild(li); // Adiciona o li à lista HTML
     }
 }
 
@@ -32,9 +32,8 @@ function adicionarAmigo() {
 nomesAmigos();
 
 function sortearAmigo() {
-    const gerarIndice = Math.floor(Math.random() * 10) + 1;
+    const gerarIndice = Math.floor(Math.random() * amigos.length) + 1; // Gera um número aleatório para o índice
     const resultadoSorteio = document.getElementById("resultado");
-    const liResultado = document.createElement("li");
-    liResultado.textContent = amigos[gerarIndice];
-    resultadoSorteio.appendChild(liResultado);
+    const nomeSorteado = amigos[gerarIndice]; // Armazena dinamicamente o nome através do índice aleatório
+    resultadoSorteio.innerHTML = `Amigo sorteado: ${nomeSorteado}` //Imprime na tela o nome do sorteado
 }
